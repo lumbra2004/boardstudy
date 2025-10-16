caac6d02f8a54d1c9d4553d1ca4b14959d8c0aba
lumbra2004 <ryanlumbra@icloud.com>
2025-10-16 11:49:47 -0500

    V3.3.02

--- version.js
+++ version.js
@@ -4,19 +4,19 @@
 // Logs version and author information to the browser console.
 
 
-var version = "3.3.01";
+var version = "3.3.02";
 var date = "15OCT2025";
 
 // Individual update dates for each section
-var studyGuideDate = "Oct 15, 2025";
+var studyGuideDate = "Oct 16, 2025";
 var quizletDate = "Oct 15, 2025";
 var publicationsDate = "Oct 14, 2025";
 var additionalGuidesDate = "Oct 14, 2025";
 
 // Individual update descriptions for each section
 /* USE // FOR NEW LINE and * for BULLET POINTS */
-var studyGuideDesc = "* Added Battle Drill 1A questions, Land Navigation questions, Movement Formation questions, and more.";
-var quizletDesc = "* Updated Quizlet to reflect current study guide.";
+var studyGuideDesc = "* Added Battle Drill 1A questions, Land Navigation questions, Movement Formation questions, pictures and more.";
+var quizletDesc = "* Updated Quizlet to match study guide changes from Oct 15, 2025.";
 var publicationsDesc = "* Added Battle Drill 9A";
 var additionalGuidesDesc = "* Added Movement Formation guide.";
 
